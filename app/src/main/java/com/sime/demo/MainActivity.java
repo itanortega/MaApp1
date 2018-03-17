ef7d111b419fa17a2ab90fe9c884e85f9c48b287
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-03-17 16:49:17 -0500

    Controles adicionales

--- app/src/main/java/com/sime/demo/MainActivity.java
+++ app/src/main/java/com/sime/demo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private SeekBar sbrBlue = null;
     private SeekBar sbrAlpha = null;
     private View vieColor = null;
+    private TextView lbl_val_red = null;
+    private TextView lbl_val_green = null;
+    private TextView lbl_val_blue = null;
+    private TextView lbl_val_alpha = null;
+    private TextView lbl_val_exa = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,11 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         sbrBlue = (SeekBar) findViewById(R.id.sbrBlue);
         sbrAlpha = (SeekBar) findViewById(R.id.sbrAlpha);
         vieColor = (View) findViewById(R.id.vieColor);
+        lbl_val_red = (TextView) findViewById(R.id.lbl_val_red);
+        lbl_val_green = (TextView) findViewById(R.id.lbl_val_green);
+        lbl_val_blue = (TextView) findViewById(R.id.lbl_val_blue);
+        lbl_val_alpha = (TextView) findViewById(R.id.lbl_val_alpha);
+        lbl_val_exa = (TextView) findViewById(R.id.lbl_val_exa);
 
         sbrRed.setOnSeekBarChangeListener(this);
         sbrGreen.setOnSeekBarChangeListener(this);
@@ -40,6 +51,14 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         int b = sbrBlue.getProgress();
 
         int color = Color.argb(a, r, g, b);
+        String ex = Integer.toHexString(color);
+
+        lbl_val_red.setText("(" + String.valueOf(r) + ")");
+        lbl_val_green.setText("(" + String.valueOf(g) + ")");
+        lbl_val_blue.setText("(" + String.valueOf(b) + ")");
+        lbl_val_alpha.setText("(" + String.valueOf(a) + ")");
+        lbl_val_exa.setText("#" + ex);
+
         vieColor.setBackgroundColor(color);
     }
 
