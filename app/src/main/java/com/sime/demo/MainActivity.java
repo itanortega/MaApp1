234e6ddd31a30812ffcd69b78cccac583b128f61
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-03-17 16:17:05 -0500

    Implementado metodo para cambiar color

--- app/src/main/java/com/sime/demo/MainActivity.java
+++ app/src/main/java/com/sime/demo/MainActivity.java
@@ -1,13 +1,55 @@
 package com.sime.demo;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar sbrRed = null;
+    private SeekBar sbrGreen = null;
+    private SeekBar sbrBlue = null;
+    private SeekBar sbrAlpha = null;
+    private View vieColor = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sbrRed = (SeekBar) findViewById(R.id.sbrRed);
+        sbrGreen = (SeekBar) findViewById(R.id.sbrGreen);
+        sbrBlue = (SeekBar) findViewById(R.id.sbrBlue);
+        sbrAlpha = (SeekBar) findViewById(R.id.sbrAlpha);
+        vieColor = (View) findViewById(R.id.vieColor);
+
+        sbrRed.setOnSeekBarChangeListener(this);
+        sbrGreen.setOnSeekBarChangeListener(this);
+        sbrBlue.setOnSeekBarChangeListener(this);
+        sbrAlpha.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int a = sbrAlpha.getProgress();
+        int r = sbrRed.getProgress();
+        int g = sbrGreen.getProgress();
+        int b = sbrBlue.getProgress();
+
+        int color = Color.argb(a, r, g, b);
+        vieColor.setBackgroundColor(color);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
